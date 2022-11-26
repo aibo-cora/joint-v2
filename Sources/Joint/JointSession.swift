726625179809b8bd25fbaf7619a70c570a1bfcfe
Yura Filatov <yura.fila@gmail.com>
2022-11-26 18:00:04 -0500

    Finishing with Transport.
    All Publishers and Errors are hooked up.

---
+++ Sources/Joint/JointSession.swift
@@ -0,0 +1,14 @@
+//
+//  JointSession.swift
+//  
+//
+//  Created by Yura on 11/26/22.
+//
+
+import Foundation
+import JointCore
+import Combine
+
+public final class JointSession: ObservableObject {
+    
+}
