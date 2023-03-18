8391a1fc79373331b02511e529ec12f7fc4fac0f
Yura Filatov <yura.fila@gmail.com>
2023-03-18 15:07:58 -0400

    Updated core. Using checksum for dependency.

--- Package.swift
+++ Package.swift
@@ -10,23 +10,18 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Joint",
-            targets: ["Joint"]),
+        .library(name: "Joint", targets: ["Joint"]),
     ],
     dependencies: [
-        .package(url: "git@github.com:aibo-cora/MQTTClient-SPM.git", from: "0.15.3")
+        .package(url: "https://github.com/aibo-cora/MQTTClient-SPM.git", from: "0.15.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "JointCore", path: "Sources/JointCore/JointCore.xcframework"),
+        .binaryTarget(name: "JointCore", url: "https://github.com/aibo-cora/joint-v2/releases/download/0.6.1/JointCore.xcframework.zip", checksum: "8cf4b983af7c4b3b5e4fc7f7494b2f3a0951eefffe4a744f53b9f2106608a006"),
         .target(
             name: "Joint",
-            dependencies: [
-                .target(name: "JointCore"),
-                .product(name: "MQTTClient", package: "mqttclient-spm")
-            ]
+            dependencies: [.target(name: "JointCore"), .product(name: "MQTTClient", package: "mqttclient-spm")]
         ),
         .testTarget(
             name: "JointTests",
